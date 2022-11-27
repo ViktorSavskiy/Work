7754137335e6ffbc18f017f92bbf80227cc6bb44
geekbrains <info@geekbrains.ru>
2022-11-27 14:15:11 +0300

    H

---
+++ 1.js
@@ -0,0 +1,4 @@
+const num = Number.parseInt(prompt("Введите температуру"));
+const num2 = (9/5)*num+32;
+
+alert(`Цельсий: ${num}, Фаренгейт: ${num2}`);
---
+++ 2.js
@@ -0,0 +1,6 @@
+const n = prompt("Введите имя");
+function hi(n){
+    return n;
+    }
+
+alert(`Привет ${hi(n)}`)
